669b3375bf2fef0fc5da9b279956b0b55293ab18
lomins <lominsp54@gmail.com>
2024-02-26 14:51:04 +0300

    initial commit

---
+++ cmd/bot/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"github.com/tn-bot/internal/telegram"
+)
+
+func main() {
+	token, ok := os.LookupEnv("TELEGRAM_APITOKEN")
+	if !ok {
+		log.Fatalf("Can't find .env or token")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	telegramBot := telegram.NewBot(bot)
+	telegramBot.Start()
+}
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
